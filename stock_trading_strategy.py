5127d1378a92deed6788f172a09344bafd97f6cb
Adam Tesluk <adam.t@bigmirrorlabs.com>
2018-11-12 10:38:52 -0500

    manipulate data to what we want

--- stock_trading_strategy.py
+++ stock_trading_strategy.py
@@ -14,3 +14,33 @@ project_helper.print_dataframe(close)
 
 apple_ticker = 'AAPL'
 project_helper.plot_stock(close[apple_ticker], '{} Stock'.format(apple_ticker))
+
+
+def resample_prices(close_prices, freq='M'):
+    """
+    Resample close prices for each ticker at specified frequency.
+
+    Parameters
+    ----------
+    close_prices : DataFrame
+        Close prices for each ticker and date
+    freq : str
+        What frequency to sample at
+        For valid freq choices, see http://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases
+
+    Returns
+    -------
+    prices_resampled : DataFrame
+        Resampled prices for each ticker and date
+    """
+    close = pd.DataFrame(close_prices.resample(freq).last())
+    return close
+
+
+project_tests.test_resample_prices(resample_prices)
+
+monthly_close = resample_prices(close)
+project_helper.plot_resampled_prices(
+    monthly_close.loc[:, apple_ticker],
+    close.loc[:, apple_ticker],
+    '{} Stock - Close Vs Monthly Close'.format(apple_ticker))

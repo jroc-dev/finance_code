7e3c3b971df8867ff0c4e3cee07189a418f63970
Adam Tesluk <adam.t@bigmirrorlabs.com>
2018-11-12 10:40:26 -0500

    here is my trading signal

--- stock_trading_strategy.py
+++ stock_trading_strategy.py
@@ -70,3 +70,78 @@ monthly_close_returns = compute_log_returns(monthly_close)
 project_helper.plot_returns(
     monthly_close_returns.loc[:, apple_ticker],
     'Log Returns of {} Stock (Monthly)'.format(apple_ticker))
+
+
+def shift_returns(returns, shift_n):
+    """
+    Generate shifted returns
+
+    Parameters
+    ----------
+    returns : DataFrame
+        Returns for each ticker and date
+    shift_n : int
+        Number of periods to move, can be positive or negative
+
+    Returns
+    -------
+    shifted_returns : DataFrame
+        Shifted returns for each ticker and date
+    """
+
+    return returns.shift(shift_n)
+
+
+project_tests.test_shift_returns(shift_returns)
+
+
+prev_returns = shift_returns(monthly_close_returns, 1)
+lookahead_returns = shift_returns(monthly_close_returns, -1)
+
+project_helper.plot_shifted_returns(
+    prev_returns.loc[:, apple_ticker],
+    monthly_close_returns.loc[:, apple_ticker],
+    'Previous Returns of {} Stock'.format(apple_ticker))
+project_helper.plot_shifted_returns(
+    lookahead_returns.loc[:, apple_ticker],
+    monthly_close_returns.loc[:, apple_ticker],
+    'Lookahead Returns of {} Stock'.format(apple_ticker))
+
+
+def get_top_n(prev_returns, top_n):
+    """
+    Select the top performing stocks
+
+    Parameters
+    ----------
+    prev_returns : DataFrame
+        Previous shifted returns for each ticker and date
+    top_n : int
+        The number of top performing stocks to get
+
+    Returns
+    -------
+    top_stocks : DataFrame
+        Top stocks for each ticker and date marked with a 1
+    """
+    # TODO: Implement Function
+    # print(prev_returns.head())
+    res = pd.DataFrame(columns=prev_returns.columns)
+
+    for index, row in prev_returns.iterrows():
+        curr_month = row
+        curr_top = pd.Series(curr_month).nlargest(top_n)
+        top = list(curr_top.index.values)
+        for col in res.columns:
+            if (col in top):
+                res.loc[index, col] = True
+            else:
+                res.loc[index, col] = False
+
+    for index, row in res.iterrows():
+        res.loc[index] = res.loc[index].astype('int64')
+    # print(res.head())
+    return res
+
+
+project_tests.test_get_top_n(get_top_n)
